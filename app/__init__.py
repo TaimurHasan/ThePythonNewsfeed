6797f36d13acd1d807f98a0051bb0ec551a57c79
TaimurHasan <taimurhasan11@gmail.com>
2022-08-13 15:34:23 -0400

    added routes in python for homepage and dashboard, add existing frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from decouple import config
+from app.routes import home, dashboard
 
 def create_app(test_config=None):
     # set up app config
@@ -12,5 +13,9 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'hello world'
+    
+    # register routes
+    app.register_blueprint(home)
+    app.register_blueprint(dashboard)
 
     return app
